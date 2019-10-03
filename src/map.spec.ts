d63eeadda8d65ff03fbd578087339c68eb8ea21e
Denis Zatsepin <denis@zatsepin.me>
2019-10-03 18:23:43 +0200

    chore: lint

--- src/map.spec.ts
+++ src/map.spec.ts
@@ -1,15 +1,15 @@
-import {map} from './map'
+import { map } from './map'
 
 describe('map operator', () => {
   it('should transform items of async iterator', async () => {
     const arr = [1, 2, 3]
     const generator = async function*() {
-      for (let i in arr) {
+      for (const i in arr) {
         yield Promise.resolve(arr[i])
       }
     }
 
-    const doubled = map(async (a) => Promise.resolve(a * 2))(generator())
+    const doubled = map(async a => Promise.resolve(a * 2))(generator())
 
     const result = []
     for await (const item of doubled) {
--- src/map.ts
+++ src/map.ts
@@ -2,9 +2,7 @@ import { Operator } from './pipe'
 
 export type Selector<S, R = S> = (value: S, index: number) => Promise<R> | R
 
-export function map<S = any, R = S>(
-  selector: Selector<S, R>
-): Operator<S, R> {
+export function map<S = any, R = S>(selector: Selector<S, R>): Operator<S, R> {
   return function operator(
     source: AsyncIterableIterator<S>
   ): AsyncIterableIterator<R> {
