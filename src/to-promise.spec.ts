2a495dcf495744a68e98aee3b0b2ee15a309bbd6
Denis Zatsepin <denis@zatsepin.me>
2019-11-15 10:13:31 +0100

    test: add toPromise test

--- src/to-promise.spec.ts
+++ src/to-promise.spec.ts
@@ -1,10 +1,17 @@
 import { as } from './as'
 import { toPromise } from './to-promise'
+import { of } from './of'
 
 describe('to-promise operator', () => {
   it('should transform iterator to promise', async () => {
     const result = await toPromise(as(1000))
 
     expect(result).toBe(1000)
+  })
+
+  it('should convert iterator to promise of the first iterator item', async () => {
+    const result = await toPromise(of(1, 2, 3))
+
+    expect(result).toBe(1)
   })
 })
