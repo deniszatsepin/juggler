3ceb319963c858a7eeb75fd5d1e00826574cd1d5
Denis Zatsepin <denis@zatsepin.me>
2019-11-15 10:23:30 +0100

    refactor: find and some tests

--- src/find.spec.ts
+++ src/find.spec.ts
@@ -1,17 +1,13 @@
 import { find } from './find'
 import { of } from './of'
 import { compose } from './compose'
-import { collect } from './collect'
 import { toPromise } from './to-promise'
 
 describe('find operator', () => {
   it('should find item in async iterator', async () => {
-    const transformator = compose(
-      find(el => el === 3),
-      collect
-    )
+    const transformator = compose(find(el => el === 3))
     const result = await toPromise(transformator(of(1, 2, 3, 4, 5)))
 
-    expect(result).toEqual([3])
+    expect(result).toEqual(3)
   })
 })
--- src/some.spec.ts
+++ src/some.spec.ts
@@ -1,17 +1,13 @@
 import { some } from './some'
 import { of } from './of'
 import { compose } from './compose'
-import { collect } from './collect'
 import { toPromise } from './to-promise'
 
 describe('some operator', () => {
   it('should check if at least one item in async iterator is satisfy the condition', async () => {
-    const transformator = compose(
-      some(el => el === 3),
-      collect
-    )
+    const transformator = compose(some(el => el === 3))
     const result = await toPromise(transformator(of(1, 2, 3, 4, 5)))
 
-    expect(result).toEqual([true])
+    expect(result).toEqual(true)
   })
 })
