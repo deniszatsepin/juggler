9595cef2b4fdb444a6aabda8e714c8bc4a9f29fc
Denis Zatsepin <denis@zatsepin.me>
2020-09-19 07:39:27 +0200

    cleanup

--- dist/to-promise.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toPromise = void 0;
-async function toPromise(source) {
-    return (await source[Symbol.asyncIterator]().next()).value;
-}
-exports.toPromise = toPromise;
-//# sourceMappingURL=to-promise.js.map
